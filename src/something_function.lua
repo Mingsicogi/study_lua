2a8e445a31d3d9895c6ff9eca1fc3b38488f37bb
Mingsicogi <codedoctor119@gmail.com>
2023-02-26 23:07:09 +0900

    function test

--- src/something_function.lua
+++ src/something_function.lua
@@ -4,10 +4,25 @@
 --- DateTime: 2023/02/26 5:31 PM
 ---
 
+local redis = require 'redis'
+local client = redis.connect('127.0.0.1', 6379)
+
 function add_two_numbers(a, b)
     return a + b
 end
 
-local result = add_two_numbers(1, 2)
+function updateRequestCountIfKeyDataExist(...)
+    for i,v in ipairs {...} do
+        local request_count = client:incr(v)
+        if request_count == 1 then
+            client:expire(v, 60)
+        end
+    end
+end
 
+local result = add_two_numbers(1, 2)
 print(result)
+
+updateRequestCountIfKeyDataExist('key1', 'key2', 'key3', 'key4')
+
+print('Finished!')
